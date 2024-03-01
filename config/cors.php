f1cf516657730d3c16f5df4b5ece703bf4921c43
SquallStrifeX <squallstrifex@gmail.com>
2024-03-01 15:58:02 +0100

    end milestone 1

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
@@ -14,21 +12,17 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
+    // 'allowed_origins' => [env('APP_FRONTEND_URL', 'http://127.0.0.1:8000')],
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
